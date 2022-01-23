3e3fd1f5dc3168de1ea23895579787fbcc5e5758
Nycole Ribeiro <rpnyck@gmail.com>
2022-01-23 20:11:00 -0300

    Update classes.php

--- classes.php
+++ classes.php
@@ -3,12 +3,39 @@
 class Estoque {
     public $produtos = [];
     public $vendas = [];
-    public $nomeEmpresa = "";
 
-    public function __construct($nomeEmpresa) {
-        $this->nomeEmpresa = $nomeEmpresa;
+    public function __construct() {
+        $this->produtos = [
+            new Produto(1, "Produto 1", 10.00, 10),
+            new Produto(2, "Produto 2", 20.00, 20),
+            new Produto(3, "Produto 3", 30.00, 30),
+            new Produto(4, "Produto 4", 40.00, 40),
+            new Produto(5, "Produto 5", 50.00, 50),
+            new Produto(6, "Produto 6", 60.00, 60),
+            new Produto(7, "Produto 7", 70.00, 70),
+            new Produto(8, "Produto 8", 80.00, 80),
+            new Produto(9, "Produto 9", 90.00, 90),
+            new Produto(10, "Produto 10", 100.00, 100),
+            new Produto(11, "Produto 11", 110.00, 110),
+            new Produto(12, "Produto 12", 120.00, 120),
+            new Produto(13, "Produto 13", 130.00, 130)
+        ];
+        $this->vendas = [
+            new Venda(1, "Produto 1", 10.00, "01/01/2021"),
+            new Venda(2, "Produto 2", 20.00, "02/01/2021"),
+            new Venda(3, "Produto 3", 30.00, "03/01/2021"),
+            new Venda(4, "Produto 4", 40.00, "04/01/2021"),
+            new Venda(5, "Produto 5", 50.00, "05/01/2021"),
+            new Venda(6, "Produto 6", 60.00, "06/01/2021"),
+            new Venda(7, "Produto 7", 70.00, "07/01/2021"),
+            new Venda(8, "Produto 8", 80.00, "08/01/2021"),
+            new Venda(9, "Produto 9", 90.00, "09/01/2021"),
+            new Venda(10, "Produto 10", 100.00, "10/01/2021"),
+            new Venda(11, "Produto 11", 110.00, "11/01/2021"),
+            new Venda(12, "Produto 12", 120.00, "12/01/2021")
+        ];
     }
-
+    
     public function adicionaProduto(Produto $produto) {
         $this->produtos[] = $produto;
     }
@@ -39,15 +66,6 @@ class Produto {
         $this->preco = $preco;
         $this->quantidade = $quantidade;
     }
-
-    public function imprimeProduto() {
-        echo "<tr>";
-        echo "<td>".$this->codigo."</td>";
-        echo "<td>".$this->nome."</td>";
-        echo "<td>".$this->preco."</td>";
-        echo "<td>".$this->quantidade."</td>";
-        echo "</tr>";
-    }
 }
 
 
@@ -61,26 +79,22 @@ class Venda {
         $this->preco = $preco;
         $this->data = $data;
     }
+}
 
-    public function imprimeVenda() {
-        echo "<tr>";
-        echo "<td>".$this->codigoVenda."</td>";
-        echo "<td>".$this->nome."</td>";
-        echo "<td>".$this->preco."</td>";
-        echo "<td>".$this->data."</td>";
-        echo "</tr>";
+
+class ItemVendido {
+    public function alteraEstoque(Produto $produto, $empresa) {
+        $listaProds = $empresa->getProdutos();
+        foreach($listaProds as $prod){
+            if($prod->codigo == $produto->codigo){
+                $prod->quantidade -= 1;
+                break;
+            }
+        }
     }
 }
 
 
-class itemVendido {
-    public $codigoVenda;
-
-    public function __construct($codigoVenda, Produto $produto) {
-        $this->codigoVenda = $codigoVenda;
-        $this->produto = $produto;
-    }
-}
-
+$empresa = new Estoque();
 
 ?>
