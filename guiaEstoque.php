020edef9aa9011e2171c4b893fcb78787c6767df
Nycole Ribeiro <rpnyck@gmail.com>
2022-01-23 20:12:22 -0300

    Update guiaEstoque.php

--- guiaEstoque.php
+++ guiaEstoque.php
@@ -26,56 +26,50 @@
 
     <main>
         <div class="mainWindow">
-            <form class="pesquisa" action="" method="POST">
-                <a href="cadastroProduto.php">
-                    <div class="botaoPreto">Novo Produto</div>
-                </a>
-                <input type="text" name="codigoEstoque" id="codigo" placeholder="Cód. do Produto">
-                <button name="buscarProduto" class="btn_busca" type="submit">
-                    <i class="fas fa-search"></i>
-                </button>
-            </form>
+            <div class="barraPrincipal">
+                <form class="pesquisa" action="" method="POST">
+                    <a href="cadastroProduto.php">
+                        <div class="botaoPreto">Novo Produto</div>
+                    </a>
+                    <input type="text" name="codigoEstoque" id="codigo" placeholder="Cód. do Produto">
+                    <button name="buscarProduto" class="btn_busca" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="lista">
+                <?php
+                    require_once('classes.php');
 
-            <?php
-                $listaProdutos = [
-                    (object)["codigo" => "1", "nome" => "Produto 1", "quantidade" => "10", "preco" => "10,00"],
-                    (object)["codigo" => "2", "nome" => "Produto 2", "quantidade" => "20", "preco" => "20,00"],
-                    (object)["codigo" => "3", "nome" => "Produto 3", "quantidade" => "30", "preco" => "30,00"],
-                    (object)["codigo" => "4", "nome" => "Produto 4", "quantidade" => "40", "preco" => "40,00"],
-                    (object)["codigo" => "5", "nome" => "Produto 5", "quantidade" => "50", "preco" => "50,00"],
-                    (object)["codigo" => "6", "nome" => "Produto 6", "quantidade" => "60", "preco" => "60,00"],
-                    (object)["codigo" => "7", "nome" => "Produto 7", "quantidade" => "70", "preco" => "70,00"],
-                    (object)["codigo" => "8", "nome" => "Produto 8", "quantidade" => "80", "preco" => "80,00"],
-                    (object)["codigo" => "9", "nome" => "Produto 9", "quantidade" => "90", "preco" => "90,00"],
-                    (object)["codigo" => "10", "nome" => "Produto 10", "quantidade" => "100", "preco" => "100,00"]
-                ];
-                echo "<table class='listaProdutos'>";
-                echo "<tr>";
-                echo "<th class='codprod'>Código</th>";
-                echo "<th class='nomeprod'>Nome</th>";
-                echo "<th class='precoprod'>Preço</th>";
-                echo "<th class='qtdprod'>Quantidade</th>";
-                echo "</tr>";
-                foreach($listaProdutos as $produto){
-                    if(intval($produto->codigo)%2 == 0){
-                        echo "<tr class='par'>";
-                        echo "<td class='codprod'>".$produto->codigo."</td>";
-                        echo "<td class='nomeprod'>".$produto->nome."</td>";
-                        echo "<td class='precoprod'>".$produto->preco."</td>";
-                        echo "<td class='qtdprod'>".$produto->quantidade."</td>";
-                        echo "</tr>";
-                    } else {
-                        echo "<tr class='impar'>";
-                        echo "<td class='codprod'>".$produto->codigo."</td>";
-                        echo "<td class='nomeprod'>".$produto->nome."</td>";
-                        echo "<td class='precoprod'>".$produto->preco."</td>";
-                        echo "<td class='qtdprod'>".$produto->quantidade."</td>";
-                        echo "</tr>";
+                    $listaProdutos = $empresa->getProdutos();
+
+                    echo "<table class='listaProdutos'>";
+                    echo "<tr>";
+                    echo "<th class='codprod'>Código</th>";
+                    echo "<th class='nomeprod'>Nome</th>";
+                    echo "<th class='precoprod'>Preço</th>";
+                    echo "<th class='qtdprod'>Quantidade</th>";
+                    echo "</tr>";
+                    foreach($listaProdutos as $produto){
+                        if(intval($produto->codigo)%2 == 0){
+                            echo "<tr class='par'>";
+                            echo "<td class='codprod'>".$produto->codigo."</td>";
+                            echo "<td class='nomeprod'>".$produto->nome."</td>";
+                            echo "<td class='precoprod'>".$produto->preco."</td>";
+                            echo "<td class='qtdprod'>".$produto->quantidade."</td>";
+                            echo "</tr>";
+                        } else {
+                            echo "<tr class='impar'>";
+                            echo "<td class='codprod'>".$produto->codigo."</td>";
+                            echo "<td class='nomeprod'>".$produto->nome."</td>";
+                            echo "<td class='precoprod'>".$produto->preco."</td>";
+                            echo "<td class='qtdprod'>".$produto->quantidade."</td>";
+                            echo "</tr>";
+                        }
                     }
-                }
-                echo "</table>";
-
-            ?>
+                    echo "</table>";
+                ?>
+            </div>
         </div>
     </main>
 </body>
